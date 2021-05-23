424e5f970b48114e51abc1f8d634fcba305a5348
Jung Lee <lee.jung.s95@gmail.com>
2021-05-23 23:47:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < Sinatra::Base
 #"show" erb page.
     post "/articles" do
         @article = Article.create(params)
-        redirect to "articles/:#{@article.id}"
+        redirect to "articles/#{@article.id}"
     end
 #read
   get "/articles/:id" do
