3b13e2271fd4d5f2f66cb2959c92c5c55a63c67f
Jung Lee <lee.jung.s95@gmail.com>
2021-05-24 00:54:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ class ApplicationController < Sinatra::Base
 
     patch '/articles/:id' do
         @article = Article.find(params[:id])
-        @article.update(params[:article])
+        @article.update
         redirect to "/articles/#{@article.id}"
     end
 end
